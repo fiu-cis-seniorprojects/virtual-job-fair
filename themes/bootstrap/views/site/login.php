38cea3a2f13ef13df9fff23eb567affb79bb34e7
manuel <mbouz002@fiu.edu>
2014-05-27 19:05:04 -0400

    testing

--- themes/bootstrap/views/site/login.php
+++ themes/bootstrap/views/site/login.php
@@ -74,6 +74,7 @@ $this->breadcrumbs=array(
 </div><!-- form -->
 <!--Author Manuel
 making the links dynamic so if the base Url changed the program won not be affected
+testing
 -->
 <div id="altlogin" >
 <p>Students may login with these:</p>
