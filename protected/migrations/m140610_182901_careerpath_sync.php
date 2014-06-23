aeb2cd07192ff240d1f2eed091da645c3e19aff7
Enio Pena Navarro <epenavarro07@gmail.com>
2014-06-22 23:00:18 -0400

    bulk and single import from CareerPath done, started the interface for API key generation, database migration included

--- protected/migrations/m140610_182901_careerpath_sync.php
+++ protected/migrations/m140610_182901_careerpath_sync.php
@@ -1,25 +1,51 @@
 <?php
 
+/*
+ * All schema changes related to the cis careerpath integration + API
+ */
+
 class m140610_182901_careerpath_sync extends CDbMigration
 {
-	public function up()
-	{
-	}
-
-	public function down()
-	{
-		echo "m140610_182901_careerpath_sync does not support migration down.\n";
-		return false;
-	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
+        $this->execute("
+                        CREATE TABLE `jobfairdb`.`api_auth` (
+                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                    `valid_key` VARCHAR(255) NOT NULL,
+                                    PRIMARY KEY (`id`),
+                                    UNIQUE INDEX `key_UNIQUE` (`valid_key` ASC));
+
+
+                        ALTER TABLE `jobfairdb`.`user`
+                        CHANGE COLUMN `first_name` `first_name` VARCHAR(45) NULL ,
+                        CHANGE COLUMN `registration_date` `registration_date` DATETIME NULL ,
+                        CHANGE COLUMN `activation_string` `activation_string` VARCHAR(45) NULL ,
+                        CHANGE COLUMN `last_name` `last_name` VARCHAR(45) NULL ;
+
+                        ALTER TABLE `jobfairdb`.`job`
+                        ADD COLUMN `posting_url` TEXT NULL DEFAULT NULL AFTER `matches_found`;
+
+        ");
+
+
 	}
 
 	public function safeDown()
 	{
+        $this->execute("
+                        DROP TABLE `jobfairdb`.`api_auth`;
+
+                        ALTER TABLE `jobfairdb`.`user`
+                        CHANGE COLUMN `first_name` `first_name` VARCHAR(45) NOT NULL ,
+                        CHANGE COLUMN `registration_date` `registration_date` DATETIME NOT NULL ,
+                        CHANGE COLUMN `activation_string` `activation_string` VARCHAR(45) NOT NULL ,
+                        CHANGE COLUMN `last_name` `last_name` VARCHAR(45) NOT NULL ;
+
+                        ALTER TABLE `jobfairdb`.`job`
+                        DROP COLUMN `posting_url`;
+        ");
+
+
+		return false;
 	}
-	*/
 }
