dd7e0eabea4b462b121573d818a8a4e89cd506de
Enio Pena Navarro <epenavarro07@gmail.com>
2014-07-23 19:30:52 -0400

    fixed search illegal character error?

--- protected/controllers/JobController.php
+++ protected/controllers/JobController.php
@@ -195,15 +195,15 @@ class JobController extends Controller
                //print_r($result);die;
                $snippets[$j] = strtolower($result['results']['result']['snippet']);
                $snippets[$j] = utf8_decode($snippets[$j]);
-               $snippets[$j] = iconv(mb_detect_encoding($snippets[$j], mb_detect_order(), true), "ISO-8859-1//IGNORE", $snippets[$j]);
-                $result['results']['result']['snippet'] = '';
+               //$snippets[$j] = iconv(mb_detect_encoding($snippets[$j], mb_detect_order(), true), "ISO-8859-1//IGNORE", $snippets[$j]);
+               $result['results']['result']['snippet'] = '';
             }
             else{
                 for ($i = 0; $i < count($result['results']['result']); $i++, $j++)
                 {
                     $snippets[$j] = strtolower($result['results']['result'][$i]['snippet']);
                     $snippets[$j] = utf8_decode($snippets[$j]);
-                    $snippets[$j] = iconv(mb_detect_encoding($snippets[$j], mb_detect_order(), true), "ISO-8859-1//IGNORE", $snippets[$j]);
+                    //$snippets[$j] = iconv(mb_detect_encoding($snippets[$j], mb_detect_order(), true), "ISO-8859-1//IGNORE", $snippets[$j]);
 
                     $result['results']['result'][$i]['snippet'] = '';
                 }
